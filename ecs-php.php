5b5e9a33f675c4fbc40d49576f81865da0a0d07f
TomasVotruba <tomas.vot@gmail.com>
2020-07-29 14:18:27 +0200

    add ecs-php tests

---
+++ ecs-php.php
@@ -0,0 +1,13 @@
+<?php
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Configuration\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::SETS, [
+        SetList::DEAD_CODE
+    ]);
+};
