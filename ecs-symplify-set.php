d2452537c5aa0e0abb56579fdc438bc9839f708b
Tomas Votruba <tomas.vot@gmail.com>
2020-11-03 16:46:18 +0100

    simplify

---
+++ ecs-symplify-set.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::SETS, [
+        SetList::SYMPLIFY,
+    ]);
+};
